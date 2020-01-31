6bd64d2ad4b9070678cfd705597bee8bf17ae781
Dana Luukko and Stephanie Podolak <stephanie@>
2020-01-30 17:03:18 -0800

    continue developing game

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
 function Roster() {
 	this.players = [],
-  this.playerId = 0 
+	this.playerId = 0, 
+	this.currentPlayer = 0 
 }
 
 Roster.prototype.addPlayer = function(player) {
@@ -10,9 +11,19 @@ Roster.prototype.addPlayer = function(player) {
 	this.players.push(player);
 }
 
-Player.prototype.checkPlayerID = function()	{
-	return (this.id === 1 ? true : false )
-;}
+function switchPlayer()  {
+	if (roster.currentPlayer === 0) {
+		roster.currentPlayer = 1;
+		return roster.players[1];
+	} else if (roster.currentPlayer === 1) {
+		roster.currentPlayer = 0;
+		return roster.players[0];
+	} else {
+	}
+};
+
+
+
 
 //Player Score Logic
 function Player(name, rollScore, playScore, totalScore) {
@@ -31,8 +42,7 @@ Player.prototype.addToScore = function(player) {
 }
 
 Player.prototype.addToPlayScore = function(player) {
-	console.log(player);
-	if (this.rollScore === 0) {
+	if (this.rollScore === 1) {
 		return this.playScore = 0;
 	} else {
 		this.playScore += this.rollScore;
@@ -40,19 +50,27 @@ Player.prototype.addToPlayScore = function(player) {
 	}
 }
 
+Player.prototype.addDiceRoll = function() {
+	var rollScore = diceRoll();
+	this.rollScore = rollScore;
+};
 
 Player.prototype.addToTotalScore = function(playScore) {
  this.totalScore += playScore;
+ this.rollScore = 0;
+ this.playScore = 0;
  return this.totalScore;
+}
+
+Player.prototype.youWon = function() {
+	if (this.totalScore >= 100) {
+		alert("you win");
+	}
 }
 //Dice Roll Logic
 
 function diceRoll() {
 	var rollScore = Math.floor((Math.random() * 6) + 1);
-		if (rollScore === 1) {
-			return rollScore = 0;
-		} 
-		console.log(rollScore);
 		return rollScore;
 };
 
@@ -65,14 +83,17 @@ var roster = new Roster();
 
 //User Interface Logic
 
-function showRoll(rollScore)	{
-	$("#roll-dice-result").text(rollScore);
-	$("#play-score").text(roster.players[0].playScore);
-
+function showRoll()	{
+	$("#roll-dice-result").text(roster.players[roster.currentPlayer].rollScore);
+	$("#play-score").text(roster.players[roster.currentPlayer].playScore);
+// 				if (rollDice === 0) {
+// 					$("roll-dice-result").text();
+// 				}
 };
 
 function showHold(totalScore) {
 $("#player-one-total-score").text(roster.players[0].totalScore);
+$("#player-two-total-score").text(roster.players[1].totalScore);
 }
 
 
@@ -82,31 +103,32 @@ $("document").ready(function() {
 		event.preventDefault();
 		var playerOneName = $("input#player-one-name").val();
 		var playerTwoName = $("input#player-two-name").val();
+		$("#player-one-name-scoreboard").text(playerOneName);
+		$("#player-two-name-scoreboard").text(playerTwoName);
 		var playerOne = new Player(playerOneName);
 		var playerTwo = new Player(playerTwoName);
 		roster.addPlayer(playerOne);
 		roster.addPlayer(playerTwo);
-		var currentPlayer = roster.players[1];
-		console.log(currentPlayer);
-		var whatisthis = currentPlayer.checkPlayerID();
-	
-		
+
+		$(".name-input-form").hide();	
+		$(".scoreboard").show();
 	});
-	
+
 	$("form#roll").submit(function() {
 		event.preventDefault();
-		var rollScore = diceRoll();
-		showRoll(rollScore);
-		roster.players[0].addToPlayScore(roster.players[0]);
-		
+		roster.players[roster.currentPlayer].addDiceRoll();
+		roster.players[roster.currentPlayer].addToPlayScore(roster.players[roster.currentPlayer]);
+		showRoll(roster.players[roster.currentPlayer].rollScore);
+		roster.players[roster.currentPlayer].youWon(roster.players[roster.currentPlayer]);
 	});
 
 	$("form#hold").submit(function()	{
 		event.preventDefault();
-		var totalScore = roster.players[0].addToTotalScore(roster.players[0].playScore);
-		currentPlayer = player.checkPlayerID();
+		var totalScore = roster.players[roster.currentPlayer].addToTotalScore(roster.players[roster.currentPlayer].playScore);
 		showHold(totalScore);
-		// console.log(roster.players[0].playScore);
+		switchPlayer();
+		showRoll();
+		
 	})
 
 });
